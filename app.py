c78648dd7848f30a741190580e73eb4221f6d4a2
aeon33system <49430069+aeon33system@users.noreply.github.com>
2026-01-04 15:11:28 +0000

    v4

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return {"測試成功了!"}
+    return {"status": "success", "message": "測試成功了!!!"}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
